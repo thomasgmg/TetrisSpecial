cc489a0fe0b0c3e910e601842e0354a3fe03908f
thomasgmg <cabralzinho.mg@gmail.com>
2025-04-05 11:17:19 +0200

    add that if you insert a name with "m" it doesn't mute/unmute

--- main.cpp
+++ main.cpp
@@ -214,7 +214,7 @@ void UpdateAudioMute()
     Vector2 mousePoint = GetMousePosition();
     // Check if the mute button is clicked
     if ((CheckCollisionPointRec(mousePoint, muteButton) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) ||
-        IsKeyPressed('M'))
+        (IsKeyPressed('M') && playerNameLength >= NAME_LEN))
     {
         isMuted = !isMuted; // Toggle mute state
 
